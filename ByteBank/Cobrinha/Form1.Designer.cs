a8ca247f6522bcb147ebc317c54001a7a0c715df
MaikeZs <Thefox1998_@hotmail.com>
2020-07-31 23:04:41 -0300

    Projeto Cobrinha

---
+++ ByteBank/Cobrinha/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Cobrinha
+{
+    partial class formulario
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formulario));
+            this.boll = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.boll)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // boll
+            // 
+            this.boll.Image = ((System.Drawing.Image)(resources.GetObject("boll.Image")));
+            this.boll.Location = new System.Drawing.Point(0, 0);
+            this.boll.Margin = new System.Windows.Forms.Padding(0);
+            this.boll.Name = "boll";
+            this.boll.Size = new System.Drawing.Size(30, 30);
+            this.boll.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.boll.TabIndex = 0;
+            this.boll.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // formulario
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(840, 600);
+            this.Controls.Add(this.boll);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formulario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.formulario_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.boll)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox boll;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ByteBank/Cobrinha/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+namespace Cobrinha
+{
+    public partial class formulario : Form
+    {
+        private SoundPlayer Som = new SoundPlayer(@"C:\Estudo\CursoCsharp\ByteBank\Cobrinha\sons\ai-pai-para.wav");
+        private const string pra_cima = "Up";
+        private const string para_baixo = "Down";
+        private const string para_direita = "Right";
+        private const string para_esquerda = "Left";
+        private int PassoHorizontal = 0;
+        public int PassoVertical = 30;
+        public int Horizontal { get; set; }
+        public int Vertical { get; set; }
+
+
+        public formulario()
+        {
+            this.AutoSize = false;
+            InitializeComponent();
+        }
+
+        private void Andar()
+        {
+            Point point = new Point(Horizontal, Vertical);
+            boll.Location = point;
+            Console.WriteLine(Size.Width);
+            Console.WriteLine(point);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (bateu())
+            {
+                Som.Play();
+                timer1.Enabled = false;
+            }
+            Horizontal += PassoHorizontal;
+            Vertical += PassoVertical;
+
+            
+            Andar();
+
+        }
+
+        private bool bateu()
+        {
+            return bateuEncima()
+                || bateuEmbaixo()
+                || bateuParedeDireita()
+                || bateuParedeEsquerda();
+        }
+
+        private bool bateuParedeEsquerda()
+        {
+            return (PassoHorizontal < 0 && boll.Location.X <= 0);
+        }
+
+        private bool bateuParedeDireita()
+        {
+            return (PassoHorizontal > 0 && boll.Location.X >= (Size.Width - boll.Size.Width));
+        }
+
+        private bool bateuEncima()
+        {
+            return (PassoVertical < 0 && boll.Location.Y <= 0);
+        }
+
+        private bool bateuEmbaixo()
+        {
+            return (PassoVertical > 0 && boll.Location.Y >= (Size.Height - boll.Size.Height));
+        }
+
+        private bool vertical()
+        {
+            return PassoHorizontal == 0;
+        }
+
+        private bool horizontal()
+        {
+            return PassoVertical == 0;
+        }
+
+        private void formulario_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyValue) 
+            {
+                case 38://pra cima
+                    virar(0,-30);
+                    break;
+                case 40:// pra baixa
+                    virar(0,30);
+                    break;
+                case 37://esquerda
+                    virar(-30,0);
+                    break;
+                case 39://direita
+                    virar(30,0);
+                    break;
+
+            }
+        }
+        private void virar(int X,int Y) 
+        {
+            PassoHorizontal = X;
+            PassoVertical = Y;
+        }
+    }
+}
